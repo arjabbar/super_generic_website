834198e7d23a2020df86ce3459b71ba720e607ee
Abdur Jabbar <arjabbar@yahoo.com>
2014-10-27 13:29:16 -0500

    Changed sock

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,9 @@ lock '3.2.1'
 
 set :application, 'super_generic_website'
 set :repo_url, 'https://github.com/arjabbar/super_generic_website.git'
-set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_type, :system # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.3'
+set :rbenv_path, '/usr/local/rbenv'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
